0ecb5fa49062c13e506b0518d6755bc5c9de510e
zaidkhan3251 <zaid89779@gmail.com>
2021-05-25 20:13:51 +0500

    implemnt signin backend

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import User from '../models/userModel.js';
+import bcrypt from 'bcryptjs'
 import expressAsyncHandler from 'express-async-handler'
 import data from '../data.js'
+import { geanerateToken } from '../utils.js';
 
 const userRouter = express.Router();
 
@@ -10,4 +12,22 @@ userRouter.get('/seed', expressAsyncHandler(async(req,res)=>{
    const createdUsers= await User.insertMany(data.users) 
    res.send({createdUsers});
 }))
+
+userRouter.post(
+   '/signin',
+   expressAsyncHandler(async(req,res)=>{
+      const user = await User.findOne({email:req.body.email})
+      if(user){
+         if (bcrypt.compareSync(req.body.password,user.password)){
+            res.send({
+               _id:user._id,
+               name:user.name,
+               email:user.email,
+               isAdmin:user.isAdmin,
+               token: geanerateToken(user)
+            })
+         }
+      }
+   })
+)
 export default userRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,40 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import userRouter from './routers/userRouter.js';
-import productRouter from './routers/productRouter.js';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import userRouter from "./routers/userRouter.js";
+import dotenv from "dotenv";
+import productRouter from "./routers/productRouter.js";
+dotenv.config();
 const app = express();
-const DB = "mongodb+srv://dbestore:dbestore123@cluster0.yvc0f.mongodb.net/estoredb?retryWrites=true&w=majority"
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const DB =
+  "mongodb+srv://dbestore:dbestore123@cluster0.yvc0f.mongodb.net/estoredb?retryWrites=true&w=majority";
 // data base user&password
 // dbestore
 // dbestore123
 
-mongoose.connect(process.env.MONGODB_URL || DB,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useCreateIndex:true,
-    useFindAndModify:false
-}).then(() => console.log("Databased connected"))
-.catch(error=>console.log(error.reason,))
-app.use(cors("*"))
-app.get('/',(req,res)=>{
-    res.send("server is ready")
-})
-app.use('/api/users',userRouter )
+mongoose
+  .connect(process.env.MONGODB_URL || DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Databased connected"))
+  .catch((error) => console.log(error.reason));
+app.use(cors("*"));
+app.get("/", (req, res) => {
+  res.send("server is ready");
+});
+app.use("/api/users", userRouter);
 
-app.use('/api/products',productRouter )
+app.use("/api/products", productRouter);
 
-app.use((err,req,res,next)=>{
-    res.status(500).send({ message : err.message})
-    
-})
-const port= 5000;
-app.listen(port,()=>{
-    console.log("server at http://localhost:5000")
-})
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+const port = 5000;
+app.listen(port, () => {
+  console.log("server at http://localhost:5000");
+});
